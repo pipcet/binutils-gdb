4ec03db61b9065a1df285741c1add9b0b2e9093f
Pip Cet <pipcet@gmail.com>
2017-09-03 11:18:55 +0000

    work around problems caused by hidden glibc symbols

--- bfd/elflink.c
+++ bfd/elflink.c
@@ -2757,7 +2757,7 @@ _bfd_elf_fix_symbol_flags (struct elf_link_hash_entry *h,
       bfd_boolean force_local;
 
       force_local = (ELF_ST_VISIBILITY (h->other) == STV_INTERNAL
-		     || ELF_ST_VISIBILITY (h->other) == STV_HIDDEN);
+		     /* || ELF_ST_VISIBILITY (h->other) == STV_HIDDEN */);
       (*bed->elf_backend_hide_symbol) (eif->info, h, force_local);
     }
 
@@ -4857,7 +4857,7 @@ error_free_dyn:
 	    switch (ELF_ST_VISIBILITY (h->other))
 	      {
 	      case STV_INTERNAL:
-	      case STV_HIDDEN:
+              //case STV_HIDDEN:
 		(*bed->elf_backend_hide_symbol) (info, h, TRUE);
 		dynsym = FALSE;
 		break;
